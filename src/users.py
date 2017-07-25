2642db87d86cc639196427deb67e795df5175d5c
Isaac <dukeisaaco@gmail.com>
2017-07-24 21:46:08 -0400

    Added in definition for class unit.

---
+++ src/users.py
@@ -0,0 +1,17 @@
+class User(object):
+    """
+    :Description: Definition for users.
+    :param name: Name of the user.
+    :type name: basestring
+    :param age: Age of the user.
+    :type age: int
+    :param weight: Weight of the user.
+    :type weight: float
+    :param eye_color: User's eye color.
+    :type eye_color: basestring
+    """
+    def __init__(self, name, age, weight, eye_color):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.eye_color = eye_color
